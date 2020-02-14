b1af5ce422e74cb562117f1ddeab8f6349e51869
Hendrik Vogel <hendrik.vogel@posteo.net>
2020-02-14 21:47:05 +0100

    Initial commit

---
+++ CI/generator.py
@@ -0,0 +1,40 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+from __future__ import print_function
+import random
+
+
+# In[4]:
+
+
+buzz = ('continuout testing', 'continuous integration', 'continuous deployment', 'continuous improvement', 'devops')
+adjectives = ('complete','modern','self-service','integrated','end-to-end')
+adverbs = ('remarkbly', 'enormously','substantially','significantly','seriously')
+verbs=('accelerates','improves','enhances','revamps','boosts')
+
+
+# In[7]:
+
+
+def sample(l,n=1):
+    result = random.sample(l,n)
+    if n==1:
+        return result[0]
+    return result
+
+def generate_buzz():
+    buzz_terms = sample(buzz,2)
+    phrase = ' '.join([sample(adjectives),buzz_terms[0],sample(adverbs),sample(verbs),buzz_terms[1]])
+    return phrase.title()
+
+
+# In[8]:
+
+
+if __name__ == "__main__":
+    print(generate_buzz())
+
